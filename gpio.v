8a28910b02bcf811b22953bdada3fffe971576a9
ruslan <rdashkin94@gmail.com>
2023-06-10 01:17:38 -0400

    modified and tested to work with act model. OLD TB IS INVALID

--- gpio.v
+++ gpio.v
@@ -2,93 +2,145 @@
 
 module gpio
 #(
-   parameter WIDTH = 8
+   parameter PORT_NUM = 8
 )(
    input  clock
   ,input  reset
 
-  ,input                  din_valid
-  ,output reg             din_ready
-  ,input      [WIDTH-1:0] din
+  ,input                      req_valid
+  ,output reg                 req_ready
 
-  ,output reg             dout_valid
-  ,input                  dout_ready
-  ,output reg [WIDTH-1:0] dout
+  ,input                      din_valid
+  ,output reg                 din_ready
+  ,input      [PORT_NUM-1:0]  din
 
-  ,output reg [WIDTH  :0] ir_valid
-  ,input      [WIDTH  :0] ir_ready
+  ,output reg                 dout_valid
+  ,input                      dout_ready
+  ,output reg [PORT_NUM-1:0]  dout
+
+  ,output reg [PORT_NUM-1:0]  ir_valid
+  ,input      [PORT_NUM-1:0]  ir_ready
   
-  ,input      [     23:0] conf_0
-  ,input      [     16:0] conf_1
+  ,input                       conf_0_valid
+  ,output reg                  conf_0_ready
+  ,input      [2*PORT_NUM-1:0] conf_0
+
+  ,input                       conf_1_valid
+  ,output reg                  conf_1_ready
+  ,input      [4*PORT_NUM-1:0] conf_1
   
-  ,inout      [WIDTH-1:0] io
+  ,inout      [PORT_NUM-1:0]  io
 );
 
-reg [WIDTH-1:0] din_buf;
+reg [2*PORT_NUM-1:0] conf_0_loc;
+reg [4*PORT_NUM-1:0] conf_1_loc;
+always @(posedge clock)
+if (reset)                              conf_0_ready <= 0;
+else if (conf_0_valid & conf_0_ready)   conf_0_ready <= 0;
+else if (conf_0_valid)                  conf_0_ready <= 1;
+else                                    conf_0_ready <= conf_0_ready;
+
+always @(posedge clock)
+if (reset)                              conf_0_loc <= 0;
+else if (conf_0_valid & conf_0_ready)   conf_0_loc <= conf_0;
+else                                    conf_0_loc <= conf_0_loc;
+
+always @(posedge clock)
+if (reset)                              conf_1_ready <= 0;
+else if (conf_1_valid & conf_1_ready)   conf_1_ready <= 0;
+else if (conf_1_valid)                  conf_1_ready <= 1;
+else                                    conf_1_ready <= conf_1_ready;
+
+always @(posedge clock)
+if (reset)                              conf_1_loc <= 0;
+else if (conf_1_valid & conf_1_ready)   conf_1_loc <= conf_1;
+else                                    conf_1_loc <= conf_1_loc;
+
+reg [PORT_NUM-1:0] din_buf;
 
 genvar i;
+generate
+for (i = 0; i < PORT_NUM; i = i + 1) begin
+  assign io[i] = conf_0_loc[i] & conf_0_loc[i+8] ? 1'bz : din_buf[i];
+end
+endgenerate
 
 generate
-for (i = 0; i < WIDTH; i = i + 1) begin
-  assign io[i] = conf_0[i] ? 1'bz : din_buf[i];
+for (i = 0; i < PORT_NUM; i = i + 1) begin
+  always @(posedge clock)
+  if (reset)                  din_buf[i] <= 0;
+  else if (!conf_0_loc[i+8])  din_buf[i] <= 0;
+  else if (din_valid)         din_buf[i] <= din[i];
+  else                        din_buf[i] <= din_buf[i];
 end
 endgenerate
 
 always @(posedge clock)
-if (reset)                      din_buf <= 0;
-else if (din_valid & din_ready) din_buf <= din;
-else                            din_buf <= din_buf;
+if (reset)                      din_ready <= 0;
+else if (din_valid & din_ready) din_ready <= 0;
+else if (din_valid)             din_ready <= 1;
+else                            din_ready <= din_ready;
 
-always @(posedge clock)
-if (reset)                                  din_ready <= 0;
-else if (!(|conf_0[15:8]) | (&conf_0[7:0])) din_ready <= 0; //at least one io is enabled output
-else if (|conf_0[15:8] & !(&conf_0[7:0]))   din_ready <= 1; //at least one io is enabled output
-else                                        din_ready <= din_ready;
+wire [PORT_NUM-1:0] io_posedge, io_negedge;
+wire [PORT_NUM-1:0] ir_cond;
+wire [PORT_NUM-1:0] ir_lo, ir_hi, ir_po, ir_ne;
+reg  [PORT_NUM-1:0] ir_stat;
+reg  [PORT_NUM-1:0] io_d;
 
-wire [WIDTH-1:0] io_posedge, io_negedge;
-wire [WIDTH-1:0] ir_cond;
 
 generate
-for (i = 0; i < WIDTH; i = i + 1) begin
-  assign io_posedge[i] = ~dout[i] &  io[i] & conf_0[i] & conf_0[i+8];
-  assign io_negedge[i] =  dout[i] & ~io[i] & conf_0[i] & conf_0[i+8];
-  assign ir_cond[i] = conf_0[i] & conf_0[i+8] & conf_0[i+16] & //port enabled as input and corresponding interrupt enabled
-        ((~conf_1[2*i+1] & ~conf_1[2*i] & ~io[i]) |
-         (~conf_1[2*i+1] &  conf_1[2*i] & io_posedge[i]) | 
-         ( conf_1[2*i+1] & ~conf_1[2*i] & io_negedge[i]) | 
-         ( conf_1[2*i+1] &  conf_1[2*i] &  io[i]));
+for (i = 0; i < PORT_NUM; i = i + 1) begin
+  always @(posedge clock)
+  if (reset)                                                    io_d[i] <= 0;
+  else if (conf_0_loc[i] & conf_0_loc[i+8] & conf_1_loc[i+16])  io_d[i] <= io[i];
+  else                                                          io_d[i] <= io_d[i];
+
+  assign io_posedge[i] = ~io_d[i] &  io[i] & conf_0_loc[i] & conf_0_loc[i+8];
+  assign ir_po[i] = ~conf_1_loc[2*i+1] &  conf_1_loc[2*i] &  io_posedge[i];
+
+  assign io_negedge[i] =  io_d[i] & ~io[i] & conf_0_loc[i] & conf_0_loc[i+8];
+  assign ir_ne[i] =  conf_1_loc[2*i+1] & ~conf_1_loc[2*i] &  io_negedge[i];
+
+  assign ir_lo[i] = ~conf_1_loc[2*i+1] & ~conf_1_loc[2*i] & ~io[i];
+  assign ir_hi[i] =  conf_1_loc[2*i+1] &  conf_1_loc[2*i] &  io[i];
+
+  assign ir_cond[i] = conf_0_loc[i] & conf_0_loc[i+8] & conf_1_loc[i+16] &
+        ((ir_lo[i]) | (ir_po[i]) | (ir_ne[i]) | (ir_hi[i]));
   
   always @(posedge clock)
   if (reset)                          ir_valid[i] <= 0;
   else if (ir_valid[i] & ir_ready[i]) ir_valid[i] <= 0;
-  else if (ir_cond[i] & !conf_1[16])  ir_valid[i] <= 1;
+  else if (ir_cond[i] & !ir_stat[i])  ir_valid[i] <= 1;
   else                                ir_valid[i] <= ir_valid[i];
+
+  always @(posedge clock)
+  if (reset)                                          ir_stat[i] <= 0;
+  else if (ir_valid[i] & ir_ready[i])                 ir_stat[i] <= 1;
+  else if (ir_stat[i] & conf_1_valid & !conf_1[i+24]) ir_stat[i] <= 0;
+  else                                                ir_stat[i] <= ir_stat[i];
 end
 endgenerate
-
-wire all_ir_cond;
-assign all_ir_cond = &(ir_cond | ~(conf_0[16:8] & conf_0[7:0])) & conf_1[16];
-always @(posedge clock)
-if (reset)                          ir_valid[8] <= 0;
-else if (ir_valid[8] & ir_ready[8]) ir_valid[8] <= 0;
-else if (all_ir_cond)               ir_valid[8] <= 1;
-else                                ir_valid[8] <= ir_valid[8];
 
 generate
-for (i = 0; i < WIDTH; i = i + 1) begin
+for (i = 0; i < PORT_NUM; i = i + 1) begin
   always @(posedge clock)
-  if (reset)                          dout[i] <= 0;
-  else if ( conf_0[i] & conf_0[i+8])  dout[i] <= io[i];
-  else if (~conf_0[i] & conf_0[i+8])  dout[i] <= din_buf[i];
-  else                                dout[i] <= dout[i];
+  if (reset)                              dout[i] <= 0;
+  else if (req_valid & !conf_0_loc[i+8])  dout[i] <= 0;
+  else if (req_valid &  conf_0_loc[i])    dout[i] <= io[i];
+  else                                    dout[i] <= dout[i];
 end
 endgenerate
 
 always @(posedge clock)
-if (reset)                                    dout_valid <= 0;
-else if (!(|conf_0[15:8]) | !(|conf_0[7:0]))  dout_valid <= 0;  //at least one io is enabled input
-else if (|conf_0[15:8] & | conf_0[7:0])       dout_valid <= 1;  //at least one io is enabled input
-else if (dout_valid & dout_ready)             dout_valid <= 0;
-else                                          dout_valid <= dout_valid;
+if (reset)                        dout_valid <= 0;
+else if (dout_valid & dout_ready) dout_valid <= 0;
+else if (req_valid)               dout_valid <= 1;
+else                              dout_valid <= dout_valid;
+
+always @(posedge clock)
+if (reset)                      req_ready <= 0;
+else if (req_valid & req_ready) req_ready <= 0;
+else if (req_valid)             req_ready <= 1;
+else                            req_ready <= req_ready;
 
 endmodule
--- gpio_top.v
+++ gpio_top.v
@@ -1,21 +1,21 @@
 `timescale 1ns/1ps
 
 /*
+  FOR PORT_NUM = 8:
+
   Config 0:
   [7:0] direction
   [15:8] enable
-  [23:16] interrupt enable
 
   Config 1
-  [16:0]  each two consecutive bits 00 - low level trigger, 
+  [15:0]  each two consecutive bits 00 - low level trigger, 
                                     01 - posedge trigger, 
                                     10 - negedge trigger,
                                     11 - high level trigger
-  [17] only when all pins trigger interrupt
+  [23:16] interrupt enable
+  [31:24] interrupt status
 
   Note:
-    1. Input pins are constantly sampled and sent to the async side
-    2. Interrupts are only sampled when enabled
     3. To clear interrupt ir handshake should occur
 */
 
@@ -30,6 +30,9 @@ module gpio_top
 
   ,inout  [PORT_NUM  -1:0]  io
 
+  ,input                    async_req_req
+  ,output                   async_req_ack
+
   ,input                    async_din_req
   ,output                   async_din_ack
   ,input  [PORT_NUM  -1:0]  async_din
@@ -38,129 +41,130 @@ module gpio_top
   ,input                    async_dout_ack
   ,output [PORT_NUM  -1:0]  async_dout
 
-  ,output [PORT_NUM    :0]  async_ir_req
-  ,input  [PORT_NUM    :0]  async_ir_ack
+  ,output [PORT_NUM  -1:0]  async_ir_req
+  ,input  [PORT_NUM  -1:0]  async_ir_ack
 
   ,input                    async_conf_0_req
   ,output                   async_conf_0_ack
-  ,input  [          23:0]  async_conf_0
+  ,input  [2*PORT_NUM-1:0]  async_conf_0
 
   ,input                    async_conf_1_req
   ,output                   async_conf_1_ack
-  ,input  [          16:0]  async_conf_1
+  ,input  [4*PORT_NUM-1:0]  async_conf_1
 );
 
 wire sync_din_valid, sync_din_ready;
 wire [PORT_NUM-1:0] sync_din;
 async_to_sync_ctrl #(
-     .DATA_WIDTH    (PORT_NUM)
-    ,.SYNC_STAGE    (SYNC_STAGE)
+   .DATA_WIDTH  (PORT_NUM)
+  ,.SYNC_STAGE  (SYNC_STAGE)
 ) async_to_sync_din (
-     .clock         (clock)
-    ,.reset         (reset)
-    ,.async_req     (async_din_req)
-    ,.async_ack     (async_din_ack)
-    ,.async_d       (async_din)
-    ,.sync_valid    (sync_din_valid)
-    ,.sync_ready    (sync_din_ready)
-    ,.sync_d        (sync_din)
+   .clock       (clock)
+  ,.reset       (reset)
+  ,.async_req   (async_din_req)
+  ,.async_ack   (async_din_ack)
+  ,.async_d     (async_din)
+  ,.sync_valid  (sync_din_valid)
+  ,.sync_ready  (sync_din_ready)
+  ,.sync_d      (sync_din)
 );
 
 wire sync_dout_valid, sync_dout_ready;
 wire [PORT_NUM-1:0] sync_dout;
 sync_to_async_ctrl #(
-     .DATA_WIDTH    (PORT_NUM)
-    ,.SYNC_STAGE    (SYNC_STAGE)
+   .DATA_WIDTH  (PORT_NUM)
+  ,.SYNC_STAGE  (SYNC_STAGE)
 ) sync_to_async_dout (
-     .clock         (clock)
-    ,.reset         (reset)
-    ,.sync_valid    (sync_dout_valid)
-    ,.sync_ready    (sync_dout_ready)
-    ,.sync_d        (sync_dout)
-    ,.async_req     (async_dout_req)
-    ,.async_ack     (async_dout_ack)
-    ,.async_d       (async_dout)
+   .clock       (clock)
+  ,.reset       (reset)
+  ,.sync_valid  (sync_dout_valid)
+  ,.sync_ready  (sync_dout_ready)
+  ,.sync_d      (sync_dout)
+  ,.async_req   (async_dout_req)
+  ,.async_ack   (async_dout_ack)
+  ,.async_d     (async_dout)
 );
 
-wire [PORT_NUM  :0] sync_ir_valid, sync_ir_ready;
+wire [PORT_NUM-1:0] sync_ir_valid, sync_ir_ready;
 genvar i;
 generate
-for (i = 0; i < PORT_NUM+1; i = i + 1) begin
-  sync_to_async_ctrl #(
-       .DATA_WIDTH    (0)
-      ,.SYNC_STAGE    (SYNC_STAGE)
-  ) sync_to_async_dout (
-       .clock         (clock)
-      ,.reset         (reset)
-      ,.sync_valid    (sync_ir_valid[i])
-      ,.sync_ready    (sync_ir_ready[i])
-      ,.sync_d        ()
-      ,.async_req     (async_ir_req[i])
-      ,.async_ack     (async_ir_ack[i])
-      ,.async_d       ()
-  );
+for (i = 0; i < PORT_NUM; i = i + 1) begin
+sync_to_async_ctrl #(
+   .DATA_WIDTH  (0)
+  ,.SYNC_STAGE  (SYNC_STAGE)
+) sync_to_async_dout (
+   .clock       (clock)
+  ,.reset       (reset)
+  ,.sync_valid  (sync_ir_valid[i])
+  ,.sync_ready  (sync_ir_ready[i])
+  ,.sync_d      ()
+  ,.async_req   (async_ir_req[i])
+  ,.async_ack   (async_ir_ack[i])
+  ,.async_d     ()
+);
 end
 endgenerate
 
 wire sync_conf_0_valid, sync_conf_0_ready;
-wire [23:0] sync_conf_0;
+wire [2*PORT_NUM-1:0] sync_conf_0;
 async_to_sync_ctrl #(
-     .DATA_WIDTH    (24)
-    ,.SYNC_STAGE    (SYNC_STAGE)
+   .DATA_WIDTH  (2*PORT_NUM)
+  ,.SYNC_STAGE  (SYNC_STAGE)
 ) async_to_sync_conf_0 (
-     .clock         (clock)
-    ,.reset         (reset)
-    ,.async_req     (async_conf_0_req)
-    ,.async_ack     (async_conf_0_ack)
-    ,.async_d       (async_conf_0)
-    ,.sync_valid    (sync_conf_0_valid)
-    ,.sync_ready    (sync_conf_0_ready)
-    ,.sync_d        (sync_conf_0)
+   .clock       (clock)
+  ,.reset       (reset)
+  ,.async_req   (async_conf_0_req)
+  ,.async_ack   (async_conf_0_ack)
+  ,.async_d     (async_conf_0)
+  ,.sync_valid  (sync_conf_0_valid)
+  ,.sync_ready  (sync_conf_0_ready)
+  ,.sync_d      (sync_conf_0)
 );
 
 wire sync_conf_1_valid, sync_conf_1_ready;
-wire [16:0] sync_conf_1;
+wire [4*PORT_NUM-1:0] sync_conf_1;
 async_to_sync_ctrl #(
-     .DATA_WIDTH    (17)
+     .DATA_WIDTH    (4*PORT_NUM)
     ,.SYNC_STAGE    (SYNC_STAGE)
 ) async_to_sync_conf_1 (
-     .clock         (clock)
-    ,.reset         (reset)
-    ,.async_req     (async_conf_1_req)
-    ,.async_ack     (async_conf_1_ack)
-    ,.async_d       (async_conf_1)
-    ,.sync_valid    (sync_conf_1_valid)
-    ,.sync_ready    (sync_conf_1_ready)
-    ,.sync_d        (sync_conf_1)
+   .clock       (clock)
+  ,.reset       (reset)
+  ,.async_req   (async_conf_1_req)
+  ,.async_ack   (async_conf_1_ack)
+  ,.async_d     (async_conf_1)
+  ,.sync_valid  (sync_conf_1_valid)
+  ,.sync_ready  (sync_conf_1_ready)
+  ,.sync_d      (sync_conf_1)
 );
 
-wire [23:0] conf_0;
-wire [16:0] conf_1;
-gpio_conf
-gpio_conf (
-     .clock         (clock)
-    ,.reset         (reset)
-    
-    ,.conf_0_valid  (sync_conf_0_valid)
-    ,.conf_0_ready  (sync_conf_0_ready)
-    ,.conf_0_in     (sync_conf_0)
-    ,.conf_0_out    (conf_0)
-
-    ,.conf_1_valid  (sync_conf_1_valid)
-    ,.conf_1_ready  (sync_conf_1_ready)
-    ,.conf_1_in     (sync_conf_1)
-    ,.conf_1_out    (conf_1)
-
+wire sync_req_valid, sync_req_ready;
+wire [2*PORT_NUM-1:0] sync_req;
+async_to_sync_ctrl #(
+     .DATA_WIDTH    (0)
+    ,.SYNC_STAGE    (SYNC_STAGE)
+) async_to_sync_req (
+   .clock       (clock)
+  ,.reset       (reset)
+  ,.async_req   (async_req_req)
+  ,.async_ack   (async_req_ack)
+  ,.async_d     ()
+  ,.sync_valid  (sync_req_valid)
+  ,.sync_ready  (sync_req_ready)
+  ,.sync_d      ()
 );
 
 gpio #(
-  .WIDTH(PORT_NUM)
+  .PORT_NUM(PORT_NUM)
 ) gpio (
-   .clock(clock)
-  ,.reset(reset)
-  ,.din_valid (sync_din_valid)
-  ,.din_ready (sync_din_ready)
-  ,.din       (sync_din)
+   .clock       (clock)
+  ,.reset       (reset)
+
+  ,.req_valid   (sync_req_valid)
+  ,.req_ready   (sync_req_ready)
+
+  ,.din_valid   (sync_din_valid)
+  ,.din_ready   (sync_din_ready)
+  ,.din         (sync_din)
 
   ,.dout_valid  (sync_dout_valid)
   ,.dout_ready  (sync_dout_ready)
@@ -169,8 +173,13 @@ gpio #(
   ,.ir_valid    (sync_ir_valid)
   ,.ir_ready    (sync_ir_ready)
 
-  ,.conf_0      (conf_0)
-  ,.conf_1      (conf_1)
+  ,.conf_0_valid(sync_conf_0_valid)
+  ,.conf_0_ready(sync_conf_0_ready)
+  ,.conf_0      (sync_conf_0)
+
+  ,.conf_1_valid(sync_conf_1_valid)
+  ,.conf_1_ready(sync_conf_1_ready)
+  ,.conf_1      (sync_conf_1)
 
   ,.io          (io)
 );
